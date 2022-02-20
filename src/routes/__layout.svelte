2f42b296b1878d6ee1c2beefb9c3c21cd4dbb83c
Noah Semus <33107099+noahsemus@users.noreply.github.com>
2022-02-19 23:05:20 -0500

    Update __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,9 @@
-<h1>Noah Semus is a designer<br /> who designs things</h1>
+<h1>
+	Noah Semus is a designer<br /> who designs things<img
+		src="../img/face/face03.png"
+		alt="Noah's face"
+	/>
+</h1>
 
 <slot />
 
@@ -11,5 +16,10 @@
 		left: 0px;
 		padding: 60px;
 		background: linear-gradient(180deg, #000000 60.42%, rgba(0, 0, 0, 0) 100%);
+	}
+
+	img {
+		margin-left: 8px;
+		height: 28px;
 	}
 </style>
